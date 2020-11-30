0af4416a1c28d36512e958856a1141fda70ca575
Mohan Kumar <mks10685@gmail.com>
2020-11-30 20:49:17 +0530

    first commit in project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React from "react";
+import bankOne from "./drumData/BankOne"
+import bankTwo from "./drumData/BankTwo";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      drumpads1: bankOne,
+      drumpads2:bankTwo,
+      bitsName: ""
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(event){
+    console.log(event.target.id);
+    this.setState({
+      bitsName: event.target.id
+    })
+    let audio = new Audio(event.target.childNodes[1].src);
+    audio.play();
+  }
+  render(){
+    return (
+      <div id="drum-machine">
+            <div id="display-drum">
+            {this.state.drumpads1.map(obj => {
+                return (<div className="drum-pad" id={obj.id} key={obj.id} onClick={this.handleClick}>
+                            {obj.keyTrigger}
+                            <audio className="clip" id={obj.keyTrigger} src={obj.url}></audio>
+                      </div>
+                      )
+              })}
+            </div>
+            <div id="accsessory">
+            <div>
+              <label for="power" className="switch">Power</label>
+              <input type="checkbox" id="power" onChange={this.handleChange}/>
+              <span class="slider round"></span>
+            </div>
+            <div className="bitsName">
+              <h1>{this.state.bitsName}</h1>
+            </div>
+            <div>
+              <label for="bank" className="switch">Bank</label>
+              <input type="checkbox" id="bank"/>
+              <span class="slider round"></span>
+            </div>
+            </div>
+      </div>
+    )
+  }
 }
 
 export default App;
